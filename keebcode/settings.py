ecd9dfaff0d8fb9998c91990990d6121d98ef088
Alexandre1999 <nestaray@hotmail.com>
2021-10-08 15:38:02 +0000

    Updated python packages

--- keebcode/settings.py
+++ keebcode/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['164.90.238.204', 'keebcode.com', 'www.keebcode.com', '127.0.0.1']
 
